47068c06b0be6f83946b72fdd527041435ac06ff
Ivan <ivanolef.io@gmail.com>
2026-01-28 01:59:15 +0200

    fixed api

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
-
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { cookies } from 'next/headers'; 
 import { checkSessionServer } from '@/lib/api/serverApi';
 
 const PRIVATE_ROUTES = ['/notes', '/profile'];
@@ -9,36 +9,55 @@ const AUTH_ROUTES = ['/sign-in', '/sign-up'];
 export async function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+  const refreshToken = cookieStore.get('refreshToken')?.value;
+
   const isPrivateRoute = PRIVATE_ROUTES.some(route => pathname.startsWith(route));
   const isAuthRoute = AUTH_ROUTES.some(route => pathname.startsWith(route));
 
-  try {
-    const apiResponse = await checkSessionServer();
-    
+  if (accessToken) {
     if (isAuthRoute) {
       return NextResponse.redirect(new URL('/notes/filter/all', request.url));
     }
-
-    const nextResponse = NextResponse.next();
-
-    const setCookieHeader = apiResponse?.headers['set-cookie'];
-    if (setCookieHeader) {
-      setCookieHeader.forEach((cookie: string) => {
-        nextResponse.headers.append('Set-Cookie', cookie);
-      });
-    }
-
-    return nextResponse;
-
-  } catch {
-    if (isPrivateRoute) {
-      return NextResponse.redirect(new URL('/sign-in', request.url));
-    }
-    
     return NextResponse.next();
   }
-}
 
+  if (!accessToken && refreshToken) {
+    try {
+      const apiResponse = await checkSessionServer();
+      const nextResponse = NextResponse.next();
+
+      const setCookieHeader = apiResponse?.headers['set-cookie'];
+      if (setCookieHeader) {
+        setCookieHeader.forEach((cookie: string) => {
+          nextResponse.headers.append('Set-Cookie', cookie);
+        });
+      }
+
+      if (isAuthRoute) {
+        return NextResponse.redirect(new URL('/notes/filter/all', request.url));
+      }
+
+      return nextResponse;
+    } catch {
+      if (isPrivateRoute) {
+        return NextResponse.redirect(new URL('/sign-in', request.url));
+      }
+    }
+  }
+
+  if (!accessToken && !refreshToken && isPrivateRoute) {
+    return NextResponse.redirect(new URL('/sign-in', request.url));
+  }
+
+  return NextResponse.next();
+}
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    '/profile/:path*',
+    '/notes/:path*',
+    '/sign-in',
+    '/sign-up',
+  ],
 };
